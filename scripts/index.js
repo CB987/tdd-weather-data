6851e5c2a0775dc1739e1253954598d6c830d3ab
Clare Barton <grimalkinclare@gmail.com>
2019-03-06 13:14:43 -0500

    functions pass all tests

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,35 @@
-
 // Implement the following functions to fulfill the tests!
-function getLocationName() {}
-function getLocationCountry() {}
-function getLocationLatitude() {}
-function getLocationLongitude() {}
-function getDescription() {}
-function getWindSpeed() {}
-function getSunrise() {}
-
+function getLocationName(atlWeather) {
+  return atlWeather.name;
+}
+function getLocationCountry(atlWeather) {
+  return atlWeather.sys.country;
+}
+function getLocationLatitude(atlWeather) {
+  return atlWeather.coord.lat;
+}
+function getLocationLongitude(atlWeather) {
+  return atlWeather.coord.lon;
+}
+function getDescription(atlWeather) {
+  return atlWeather.weather[0].description;
+}
+function getWindSpeed(atlWeather) {
+  return atlWeather.wind.speed;
+}
+function getSunrise(atlWeather) {
+  return atlWeather.sys.sunrise;
+}
 
 // Please ignore the following
 try {
-    module.exports = {
-        getLocationName,
-        getLocationCountry,
-        getLocationLatitude,
-        getLocationLongitude,
-        getDescription,
-        getWindSpeed,
-        getSunrise
-    }
-} catch (e) {
-    
-}
+  module.exports = {
+    getLocationName,
+    getLocationCountry,
+    getLocationLatitude,
+    getLocationLongitude,
+    getDescription,
+    getWindSpeed,
+    getSunrise
+  };
+} catch (e) {}
